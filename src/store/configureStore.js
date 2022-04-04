9209dfe44ac54a338a0430fecee89b81b8d4c38d
sboursen <dev.sboursen@gmail.com>
2022-04-04 16:37:07 +0100

    Setup basic redux store

---
+++ src/store/configureStore.js
@@ -0,0 +1,29 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+  compose,
+} from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import profileReducer from './profile/Profile';
+import rocketsReducer from './rockets/Rockets';
+import missionsReducer from './missions/Missions';
+
+const rootReducer = combineReducers({
+  profile: profileReducer,
+  rockets: rocketsReducer,
+  missions: missionsReducer,
+});
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+/* eslint-enable */
+
+export default store;
