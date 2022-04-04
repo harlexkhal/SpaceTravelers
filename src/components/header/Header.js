44ac0e71011134eb42fc29a4ce9e595df5723390
sboursen <dev.sboursen@gmail.com>
2022-04-04 15:12:28 +0100

    Add profile route and give it basic style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,15 +3,20 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../logo.png';
 
 const Header = () => (
-  <header>
-    <NavLink to="/">
-      <img src={logo} alt="logo" />
-      {'Space Travelers\' Hub'}
+  <header className="h-16 w-[calc(100% - 3rem)] flex flex-row justify-between items-center my-6 mx-12 border-b-2 border-black pb-4">
+    <NavLink
+      className="h-full flex flex-row gap-3 justify-between items-center"
+      to="/"
+    >
+      <img className="h-full" src={logo} alt="logo" />
+      <span className="text-3xl">
+        Space Travelers&apos; Hub
+      </span>
     </NavLink>
-    <nav>
+    <nav className="flex flex-row gap-3 justify-between items-center">
       <NavLink to="/">Rocket</NavLink>
       <NavLink to="/mission">Mission</NavLink>
-      <hr />
+      <div className="w-2 h-4 border-r-2 border-black" />
       <NavLink to="/profile">Profile</NavLink>
     </nav>
   </header>
