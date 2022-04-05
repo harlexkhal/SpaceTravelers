aa4fd3aa0388453d864091e43ac39fd842762c23
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-04-05 14:41:24 +0100

    fix card margin-bottom

--- src/components/rocket/rocketCard.js
+++ src/components/rocket/rocketCard.js
@@ -5,19 +5,15 @@ import Badge from '../badge/Badge';
 import rocket from '../../rocket.jpg';
 
 const RocketCard = ({
-  id, reservaton, name, description,
+  reservaton, name, description,
 }) => (
-  <div className="flex flex-row h-40 ml-10 w-11/12">
+  <div className="flex flex-row h-40 ml-10 w-11/12 mb-5">
     <img src={rocket} alt="rocket-img" />
     <div className="ml-2.5">
-      <h1 className="font-semibold">Falcon 9</h1>
+      <h1 className="font-semibold">{name}</h1>
       <Badge twClasses={reservaton ? 'bg-blue-400' : 'hidden'} text="Reserved" />
       <p>
-        Ready for launch jska kjasj k akf ak ak ak  kjs ak kajf
-        ak f jkajfa kj  aj skfj la jka a jska  jskfjsk
-        sk j ask fa kaja ljkisa ja jfa kj kak kja fkjfas jak fjk ak
-        jfk ka fkajk f ksa f aj fajfkajf jf akj ajf ajs
-        akjflaj fjf jksjal fjl jfa
+       {description}
       </p>
       <Button
         twClasses="bg-blue-500 text-white mt-2"
--- src/routes/rockets/Rockets.js
+++ src/routes/rockets/Rockets.js
@@ -2,7 +2,10 @@ import React from 'react';
 import RocketCard from '../../components/rocket/rocketCard';
 
 const Rockets = () => (
-  <RocketCard id={1} reservaton name="Falcon 9" description="kfjksjakjfkasjf" />
+  <>
+    <RocketCard id={1} reservaton name="Falcon 9" description="kfjksjakjfkasjf" />
+    <RocketCard id={2} reservaton name="Falcon 7" description="kfjksjakjfkasjf" />
+  </>
 );
 
 export default Rockets;
