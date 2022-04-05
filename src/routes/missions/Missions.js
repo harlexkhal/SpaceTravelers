79e1b7bd3e07243ef78478f4b79b1a9c61703bd9
sboursen <dev.sboursen@gmail.com>
2022-04-05 15:44:47 +0100

    Update the style of the Table

--- src/routes/missions/Missions.js
+++ src/routes/missions/Missions.js
@@ -18,33 +18,33 @@ const Missions = () => {
     <main className="mt-6 px-12 w-full">
       <table className="table-fixed w-full border-2">
         <thead>
-          <tr>
-            <th>Mission</th>
-            <th>Description</th>
-            <th>Status</th>
+          <tr className="bg-gray-500 text-white text-left">
+            <th className="pl-2">Mission</th>
+            <th className="pl-2">Description</th>
+            <th className="pl-2">Status</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="divide-y divide-grey-500">
           {missionsList.map((dataEntry) => (
             <tr
               key={dataEntry.id}
-              className="even:bg-gray-100 border-2 border-gray-400"
+              className="even:bg-gray-100"
             >
-              <td className=" border-2 p-2 align-top">
+              <td className=" border-2 p-3 align-top text-2xl font-medium">
                 {dataEntry.mission}
               </td>
-              <td className=" border-2 p-2">
+              <td className=" border-2 p-3">
                 {dataEntry.description}
               </td>
-              <td className=" border-2 p-2">
+              <td className=" border-2 p-3">
                 <div className="grid grid-cols-2  p-2 justify-items-center items-center">
                   <Badge
                     text={dataEntry.status[0]}
-                    twClasses="bg-sky-500 text-md w-fit"
+                    twClasses="text-base px-2 bg-gray-500 text-white font-medium"
                   />
                   <Button
                     text={dataEntry.status[1]}
-                    twClasses="border-sky-500 border-2 text-md w-fit"
+                    twClasses="border-2 text-lg font-medium w-fit px-2 py-1 border-gray-500 text-gray-500"
                   />
                 </div>
               </td>
