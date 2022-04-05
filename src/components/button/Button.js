5f221f8aa6df53f65b5b39fe0c2835316f726a1a
sboursen <dev.sboursen@gmail.com>
2022-04-05 16:05:49 +0100

    Add id prop to Button component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 export default function Button({ ...props }) {
   return (
     <button
+      id={props.id}
       className={`rounded min-w-fit capitalize ${props.twClasses}`}
       type={props.type === 'submit' ? 'submit' : 'button'}
       onClick={props.handleClick}
@@ -21,6 +22,7 @@ Button.defaultProps = {
 };
 
 Button.propTypes = {
+  id: PropTypes.string.isRequired,
   type: PropTypes.string,
   text: PropTypes.string,
   twClasses: PropTypes.string,
