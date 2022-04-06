db886aec9b5e41df4ac3c060da7217374acf3fcd
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-04-06 02:48:49 +0100

    add useDispatch and useSelectors for rockets

--- src/routes/rockets/Rockets.js
+++ src/routes/rockets/Rockets.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {  useSelector, useDispatch } from 'react-redux';
+import { fetchAllRockets } from '../../store/rockets/Rockets';
 import RocketCard from '../../components/rocket/rocketCard';
 
 const Rockets = () => {
-  
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllRockets());
+  }, []);
+
+  useSelector((state) => console.log(state));
+
+  //console.log(rockets);
   return (
     <>
     <RocketCard id={1} reservaton name="Falcon 9" description="kfjksjakjfkasjf" />
