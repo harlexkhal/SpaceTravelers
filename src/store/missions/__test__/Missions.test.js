44393bad262f5bd181eb26f256bbdbc4e3c38c42
sboursen <dev.sboursen@gmail.com>
2022-04-06 13:18:15 +0100

    Test the joinMission and leaveMission action creators

--- src/store/missions/__test__/Missions.test.js
+++ src/store/missions/__test__/Missions.test.js
@@ -5,6 +5,8 @@ import missionsReducer, {
   getMissionsRequest,
   getMissionsSuccess,
   getMissionsFailure,
+  joinMission,
+  leaveMission,
 } from '../Missions';
 
 const initialState = {
@@ -70,6 +72,148 @@ describe('missionsReducer', () => {
 
       expect(store.getState().loading).toBeFalsy();
       expect(store.getState().error).toEqual(error);
+    });
+  });
+
+  describe('joinMission and leaveMission', () => {
+    afterEach(cleanup);
+
+    it('should return the correct state after joining one mission', () => {
+      const store = createStore(
+        missionsReducer,
+        applyMiddleware(thunk),
+      );
+      const mockMissionData = [
+        {
+          id: 1,
+          mission: 'mission-1',
+          description: 'mission 1 description',
+          reserved: false,
+        },
+        {
+          id: 2,
+          mission: 'mission-2',
+          description: 'mission 2 description',
+          reserved: false,
+        },
+        {
+          id: 3,
+          mission: 'mission-3',
+          description: 'mission 3 description',
+          reserved: false,
+        },
+      ];
+      store.dispatch(getMissionsSuccess(mockMissionData));
+
+      store.dispatch(joinMission(3));
+
+      expect(
+        store.getState().missionsList[2].reserved,
+      ).toBeTruthy();
+    });
+
+    it('should return the correct state after joining two mission', () => {
+      const store = createStore(
+        missionsReducer,
+        applyMiddleware(thunk),
+      );
+      const mockMissionData = [
+        {
+          id: 1,
+          mission: 'mission-1',
+          description: 'mission 1 description',
+          reserved: false,
+        },
+        {
+          id: 2,
+          mission: 'mission-2',
+          description: 'mission 2 description',
+          reserved: false,
+        },
+        {
+          id: 3,
+          mission: 'mission-3',
+          description: 'mission 3 description',
+          reserved: false,
+        },
+      ];
+      store.dispatch(getMissionsSuccess(mockMissionData));
+
+      store.dispatch(joinMission(1));
+      store.dispatch(joinMission(3));
+
+      expect(store.getState().missionsList).toEqual([
+        {
+          id: 1,
+          mission: 'mission-1',
+          description: 'mission 1 description',
+          reserved: true,
+        },
+        {
+          id: 2,
+          mission: 'mission-2',
+          description: 'mission 2 description',
+          reserved: false,
+        },
+        {
+          id: 3,
+          mission: 'mission-3',
+          description: 'mission 3 description',
+          reserved: true,
+        },
+      ]);
+    });
+
+    it('should return the correct state after joining one mission and leaving it', () => {
+      const store = createStore(
+        missionsReducer,
+        applyMiddleware(thunk),
+      );
+      const mockMissionData = [
+        {
+          id: 1,
+          mission: 'mission-1',
+          description: 'mission 1 description',
+          reserved: false,
+        },
+        {
+          id: 2,
+          mission: 'mission-2',
+          description: 'mission 2 description',
+          reserved: false,
+        },
+        {
+          id: 3,
+          mission: 'mission-3',
+          description: 'mission 3 description',
+          reserved: false,
+        },
+      ];
+      store.dispatch(getMissionsSuccess(mockMissionData));
+
+      store.dispatch(joinMission(1));
+      store.dispatch(leaveMission(1));
+
+      expect(store.getState().missionsList).toEqual([
+        {
+          id: 1,
+          mission: 'mission-1',
+          description: 'mission 1 description',
+          reserved: false,
+        },
+        {
+          id: 2,
+          mission: 'mission-2',
+          description: 'mission 2 description',
+          reserved: false,
+        },
+        {
+          id: 3,
+          mission: 'mission-3',
+          description: 'mission 3 description',
+          reserved: false,
+        },
+      ]);
     });
   });
 });
