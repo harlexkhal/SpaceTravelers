68ff430c2de5119d1de77d2d145b46607d443b07
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-04-07 17:32:17 +0100

    add hashrouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
 import './index.css';
